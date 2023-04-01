0ee0074b03b42325a249bbcb20c54adf787501a7
Wojciech M. Wnuk <laniusone@pm.me>
2023-04-01 12:51:36 +0200

    More Fzf bindings

--- lua/sigma/plugins/fzf.lua
+++ lua/sigma/plugins/fzf.lua
@@ -6,18 +6,29 @@
 --        /____/
 -- lua/sigma/plugins/fzf.lua
 
-local utils = require('sigma.utils')
+local utils, opts = require('sigma.utils'), { silent = true }
 local M = {
-    setup = function ()
-        utils.noremap('n', '<leader>ff', '<Cmd>FzfLua files<CR>')
-        utils.noremap('n', '<leader>fr', '<Cmd>FzfLua oldfiles<CR>')
-        utils.noremap('n', '<leader>rg', [[<Cmd>lua require('fzf-lua').live_grep({ cmd = "rg -g '!{.git,node_modules}/' --hidden --no-ignore --column", search = "", fzf_opts = { ['--nth'] = '2..' } })<CR>]])
-        utils.noremap('n', '<leader>bi', '<Cmd>FzfLua buffers<CR>')
-        utils.noremap('n', '<leader>cp', '<Cmd>FzfLua commands<CR>')
-        utils.noremap('n', '<leader>ll', '<Cmd>FzfLua lines<CR>')
-        utils.noremap('n', '<leader>gl', '<Cmd>FzfLua blines<CR>')
-        utils.noremap('n', '<leader>gj', '<Cmd>FzfLua jumps<CR>')
-        utils.noremap('n', '<leader>km', '<Cmd>FzfLua keymaps<CR>')
+    setup = function()
+        utils.noremap('n', '<leader>ff', '<Cmd>FzfLua files<CR>', opts)
+        utils.noremap('n', '<leader>fr', '<Cmd>FzfLua oldfiles<CR>', opts)
+        utils.noremap('n', '<leader>rg',
+        [[<Cmd>lua require('fzf-lua').live_grep({ cmd = "rg -g '!{.git,node_modules}/' --hidden --no-ignore --column", search = "", fzf_opts = { ['--nth'] = '2..' } })<CR>]],
+        opts)
+        utils.noremap('n', '<leader>bi', '<Cmd>FzfLua buffers<CR>', opts)
+        utils.noremap('n', '<leader>cp', '<Cmd>FzfLua commands<CR>', opts)
+        utils.noremap('n', '<leader>ll', '<Cmd>FzfLua lines<CR>', opts)
+        utils.noremap('n', '<leader>gl', '<Cmd>FzfLua blines<CR>', opts)
+        utils.noremap('n', '<leader>gj', '<Cmd>FzfLua jumps<CR>', opts)
+        utils.noremap('n', '<leader>km', '<Cmd>FzfLua keymaps<CR>', opts)
+        utils.noremap('n', '<leader>fzf', '<Cmd>FzfLua<CR>', opts)
+        utils.noremap('n', '<leader>fzr', '<Cmd>FzfLua resume<CR>', opts)
+        utils.noremap('n', '<leader>fzs', '<Cmd>FzfLua git_status<CR>', opts)
+        utils.noremap('n', '<leader>fzc', '<Cmd>FzfLua git_commits<CR>', opts)
+        utils.noremap('n', '<leader>fzb', '<Cmd>FzfLua git_branches<CR>', opts)
+        utils.noremap('n', '<leader>fzd', '<Cmd>FzfLua changes<CR>', opts)
+        utils.noremap('n', '<leader>ch', '<Cmd>FzfLua command_history<CR>', opts)
+        utils.noremap('n', '<leader>ht', '<Cmd>FzfLua help_tags<CR>', opts)
+        utils.noremap('n', '<leader>sh', '<Cmd>FzfLua search_history<CR>', opts)
     end
 }
 
